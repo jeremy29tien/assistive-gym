7ce77ae8e2f307ee9137cc8e7c74393d72982867
Jeremy Tien <jeremy29tien@gmail.com>
2021-11-11 19:02:26 -0800

    add code to generate trajectory data for new handpicked feature space

--- trex/noisy_pretrained.py
+++ trex/noisy_pretrained.py
@@ -57,6 +57,7 @@ for i, noise_level in enumerate(noise_levels):
         reward_over_time = []
         total_reward = 0
         observation = env.reset()
+        info = None
         done = False
         while not done:
             if COOP:
@@ -81,7 +82,19 @@ for i, noise_level in enumerate(noise_levels):
                 # Collect the data
                 # print("Observation:", observation)
                 # print("Action:", action)
-                data = np.concatenate((observation, action))
+
+                # Raw state: observation + action
+                # data = np.concatenate((observation, action))
+
+                # Handtuned features: spoon-mouth distance, amount of food particles in mouth, amount of food particles on the floor
+                distance = np.linalg.norm(observation[7:10])
+                if info is None:
+                    foods_in_mouth = 0
+                    foods_on_floor = 0
+                else:
+                    foods_in_mouth = info['foods_in_mouth']
+                    foods_on_floor = info['foods_on_ground']
+                data = np.array([distance, foods_in_mouth, foods_on_floor])
 
                 # Step the simulation forward using the action from our trained policy
                 observation, reward, done, info = env.step(action)
@@ -98,7 +111,7 @@ for i, noise_level in enumerate(noise_levels):
         cum_rewards_over_time[i].append(cum_reward_over_time)
         rewards_per_noise_level[i].append(total_reward)
 
-        print(total_reward)
+        # print(total_reward)
         total_rewards.append(total_reward)
         rewards_over_time.append(reward_over_time)
 
@@ -122,32 +135,33 @@ with np.printoptions(precision=3):
     print(rewards_per_noise_level)
     print(mean_rewards_per_noise_level)
 
-plt.figure()
-
-plt.subplot(231)
-for p in cum_rewards_over_time[0]:
-    plt.plot(p, 'b')
-
-plt.subplot(232)
-for p in cum_rewards_over_time[1]:
-    plt.plot(p, 'b')
-
-plt.subplot(233)
-for p in cum_rewards_over_time[2]:
-    plt.plot(p, 'b')
-
-plt.subplot(234)
-for p in cum_rewards_over_time[3]:
-    plt.plot(p, 'b')
-
-plt.subplot(235)
-for p in cum_rewards_over_time[4]:
-    plt.plot(p, 'b')
-
-plt.subplot(236)
-for p in cum_rewards_over_time[5]:
-    plt.plot(p, 'b')
-
-plt.savefig("cum_rewards_over_time.png")
-plt.show()
+# Code for plotting how cumulative reward changes over time with each level of noise.
+# plt.figure()
+#
+# plt.subplot(231)
+# for p in cum_rewards_over_time[0]:
+#     plt.plot(p, 'b')
+#
+# plt.subplot(232)
+# for p in cum_rewards_over_time[1]:
+#     plt.plot(p, 'b')
+#
+# plt.subplot(233)
+# for p in cum_rewards_over_time[2]:
+#     plt.plot(p, 'b')
+#
+# plt.subplot(234)
+# for p in cum_rewards_over_time[3]:
+#     plt.plot(p, 'b')
+#
+# plt.subplot(235)
+# for p in cum_rewards_over_time[4]:
+#     plt.plot(p, 'b')
+#
+# plt.subplot(236)
+# for p in cum_rewards_over_time[5]:
+#     plt.plot(p, 'b')
+#
+# plt.savefig("cum_rewards_over_time.png")
+# plt.show()
 
