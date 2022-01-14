799b82b941e9af29bfd8cc9a6d69ad6125a02a9e
Jeremy Tien <jeremy29tien@gmail.com>
2022-01-14 00:55:06 -0800

    add changes from linear model to standard model

--- trex/model.py
+++ trex/model.py
@@ -12,51 +12,70 @@ from sklearn.model_selection import train_test_split
 
 # num_trajs specifies the number of trajectories to use in our training set
 # pair_delta=1 recovers original (just that pairwise comps can't be the same)
-def create_training_data(demonstrations, num_trajs, pair_delta):
+# if all_pairs=True, rather than generating num_trajs pairwise comps with pair_delta ranking difference,
+# we simply generate all (num_demos choose 2) possible pairs from the dataset.
+def create_training_data(demonstrations, num_trajs, pair_delta, all_pairs=False):
     # collect training data
     max_traj_length = 0
     training_obs = []
     training_labels = []
     num_demos = len(demonstrations)
 
-    # add full trajs (for use on Enduro)
-    for n in range(num_trajs):
-        ti = 0
-        tj = 0
-        # only add trajectories that are different returns
-        while abs(ti - tj) < pair_delta:
-            # pick two random demonstrations
-            ti = np.random.randint(num_demos)
-            tj = np.random.randint(num_demos)
-        # create random partial trajs by finding random start frame and random skip frame
-        # si = np.random.randint(6)
-        # sj = np.random.randint(6)
-        # step = np.random.randint(3, 7)
+    if all_pairs:
+        for ti in range(num_demos):
+            for tj in range(ti+1, num_demos):
+                traj_i = demonstrations[ti]
+                traj_j = demonstrations[tj]
 
-        traj_i = demonstrations[ti]
-        traj_j = demonstrations[tj]
+                # In other words, label = (traj_i < traj_j)
+                if ti > tj:
+                    label = 0  # 0 indicates that traj_i is better than traj_j
+                else:
+                    label = 1  # 1 indicates that traj_j is better than traj_i
 
-        # In other words, label = (traj_i < traj_j)
-        if ti > tj:
-            label = 0  # 0 indicates that traj_i is better than traj_j
-        else:
-            label = 1  # 1 indicates that traj_j is better than traj_i
+                training_obs.append((traj_i, traj_j))
+                training_labels.append(label)
 
-        training_obs.append((traj_i, traj_j))
-        training_labels.append(label)
+                # We shouldn't need max_traj_length, since all our trajectories our fixed at length 200.
+                max_traj_length = max(max_traj_length, len(traj_i), len(traj_j))
+    else:
+        # add full trajs
+        for n in range(num_trajs):
+            ti = 0
+            tj = 0
+            # only add trajectories that are different returns
+            while abs(ti - tj) < pair_delta:
+                # pick two random demonstrations
+                ti = np.random.randint(num_demos)
+                tj = np.random.randint(num_demos)
 
-        # We shouldn't need max_traj_length, since all our trajectories our fixed at length 200.
-        max_traj_length = max(max_traj_length, len(traj_i), len(traj_j))
+            traj_i = demonstrations[ti]
+            traj_j = demonstrations[tj]
+
+            # In other words, label = (traj_i < traj_j)
+            if ti > tj:
+                label = 0  # 0 indicates that traj_i is better than traj_j
+            else:
+                label = 1  # 1 indicates that traj_j is better than traj_i
+
+            training_obs.append((traj_i, traj_j))
+            training_labels.append(label)
+
+            # We shouldn't need max_traj_length, since all our trajectories our fixed at length 200.
+            max_traj_length = max(max_traj_length, len(traj_i), len(traj_j))
 
     print("maximum traj length", max_traj_length)
     return training_obs, training_labels
 
 
-observation_dim = 25
-input_dim = 32  # NOTE: the input is comprised of state-action pairs, not just states (states have dim of 25, actions have dim of 7)
+# NOTE:
+# If input is comprised of state-action pairs, input_dim = 32
+# If input is comprised of states, input_dim = 25
+input_dim = 25
+
 
 class Net(nn.Module):
-    def __init__(self, with_bias=True):
+    def __init__(self, with_bias=False):
         super().__init__()
 
         self.fc1 = nn.Linear(input_dim, 128)
@@ -87,7 +106,7 @@ def learn_reward(reward_network, optimizer, training_inputs, training_outputs, n
     # check if gpu available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # Assume that we are on a CUDA machine, then this should print a CUDA device:
-    print(device)
+    print("device:", device)
     loss_criterion = nn.CrossEntropyLoss()
 
     cum_loss = 0.0
@@ -100,7 +119,6 @@ def learn_reward(reward_network, optimizer, training_inputs, training_outputs, n
         for i in range(len(training_labels)):
             traj_i, traj_j = training_obs[i]
 
-            # Question: why was it called labels?
             label = np.array([training_labels[i]])
             traj_i = np.array(traj_i)
             traj_j = np.array(traj_j)
@@ -125,11 +143,12 @@ def learn_reward(reward_network, optimizer, training_inputs, training_outputs, n
             item_loss = loss.item()
             cum_loss += item_loss
             val_loss = calc_val_loss(reward_network, val_obs, val_labels)
+            val_acc = calc_accuracy(reward_network, val_obs, val_labels)
             if i % 100 == 99:
-                # print(i)
                 print("epoch {}:{} loss {}, val_loss {}".format(epoch, i, cum_loss, val_loss))
                 cum_loss = 0.0
                 print("check pointing")
+                print("Weights:", reward_net.state_dict())
                 torch.save(reward_net.state_dict(), checkpoint_dir)
 
             # Early Stopping
@@ -138,6 +157,7 @@ def learn_reward(reward_network, optimizer, training_inputs, training_outputs, n
                 # print('trigger times:', trigger_times)
                 if trigger_times >= patience:
                     print("Early stopping.")
+                    print("Trained Weights:", reward_net.state_dict())
                     return
             else:
                 trigger_times = 0
@@ -145,6 +165,7 @@ def learn_reward(reward_network, optimizer, training_inputs, training_outputs, n
 
             prev_val_loss = val_loss
     print("finished training")
+    print("Trained Weights:", reward_net.state_dict())
 
 
 def calc_val_loss(reward_network, training_inputs, training_outputs):
@@ -214,29 +235,30 @@ if __name__ == "__main__":
                         help="name and location for learned model params, e.g. ./learned_models/breakout.params")
     parser.add_argument('--seed', default=0, help="random seed for experiments")
     parser.add_argument('--num_trajs', default=0, type=int, help="number of pairwise comparisons")
+    parser.add_argument('--num_demos', default=120, type=int, help="the number of demos to sample pairwise comps from")
     parser.add_argument('--num_epochs', default=100, type=int, help="number of training epochs")
     parser.add_argument('--lr', default=0.00005, type=float, help="learning rate")
     parser.add_argument('--weight_decay', default=0.0, type=float, help="weight decay")
-    parser.add_argument('--no_bias', dest='no_bias', default=False, action='store_true', help="whether we include a bias in the last layer")  # NOTE: type=bool doesn't work, value is still true.
+    parser.add_argument('--with_bias', dest='with_bias', default=False, action='store_true', help="whether we include a bias in the last layer")  # NOTE: type=bool doesn't work, value is still true.
     parser.add_argument('--patience', default=100, type=int, help="number of iterations we wait before early stopping")
+    parser.add_argument('--pair_delta', default=10, type=int, help="min difference between trajectory rankings in our dataset")
+    parser.add_argument('--all_pairs', dest='all_pairs', default=False, action='store_true', help="whether we generate all pairs from the dataset (num_demos choose 2)")  # NOTE: type=bool doesn't work, value is still true.
     args = parser.parse_args()
-
-    num_trajs = args.num_trajs
-    # num_snippets = args.num_snippets
-    # min_snippet_length = 50  # min length of trajectory for training comparison
-    # maximum_snippet_length = 100
 
     seed = args.seed
     torch.manual_seed(seed)
 
     ## HYPERPARAMS ##
+    num_trajs = args.num_trajs
+    num_demos = args.num_demos
     lr = args.lr
     weight_decay = args.weight_decay
     num_iter = args.num_epochs  # num times through training data
-    l1_reg = 0.0
     patience = args.patience
-    pair_delta = 10
-    no_bias = args.no_bias
+    pair_delta = args.pair_delta
+    all_pairs = args.all_pairs
+    with_bias = args.with_bias
+    l1_reg = 0.0
     #################
 
     # sort the demonstrations according to ground truth reward to simulate ranked demos
@@ -244,10 +266,15 @@ if __name__ == "__main__":
     demo_rewards = np.load("data/raw_data/demo_rewards.npy")
     demo_reward_per_timestep = np.load("data/raw_data/demo_reward_per_timestep.npy")
 
+    # Subsample the demos according to num_demos
+    # Source: https://stackoverflow.com/questions/50685409/select-n-evenly-spaced-out-elements-in-array-including-first-and-last
+    idx = np.round(np.linspace(0, len(demos) - 1, num_demos)).astype(int)
+    demos = demos[idx]
+    demo_rewards = demo_rewards[idx]
+    demo_reward_per_timestep = demo_reward_per_timestep[idx]
+
     demo_lengths = 200  # fixed horizon of 200 timesteps in assistive gym
     print("demo lengths", demo_lengths)
-    # max_snippet_length = min(np.min(demo_lengths), maximum_snippet_length)
-    # print("max snippet length", max_snippet_length)
 
     print("demos:", demos.shape)
     print("demo_rewards:", demo_rewards.shape)
@@ -260,8 +287,13 @@ if __name__ == "__main__":
     print(sorted_demo_rewards)
 
     train_val_split_seed = 100
-    obs, labels = create_training_data(sorted_demos, num_trajs, pair_delta)
-    training_obs, val_obs, training_labels, val_labels = train_test_split(obs, labels, test_size=0.10, random_state=train_val_split_seed)
+    obs, labels = create_training_data(sorted_demos, num_trajs, pair_delta, all_pairs)
+    if len(obs) > 1:
+        training_obs, val_obs, training_labels, val_labels = train_test_split(obs, labels, test_size=0.10, random_state=train_val_split_seed)
+    else:
+        print("WARNING: Since there is only one training point, the validation data is the same as the training data.")
+        training_obs = val_obs = obs
+        training_labels = val_labels = labels
 
     print("num training_obs", len(training_obs))
     print("num training_labels", len(training_labels))
@@ -270,7 +302,7 @@ if __name__ == "__main__":
 
     # Now we create a reward network and optimize it using the training data.
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    reward_net = Net(with_bias=(not no_bias))
+    reward_net = Net(with_bias=with_bias)
     reward_net.to(device)
     import torch.optim as optim
 
