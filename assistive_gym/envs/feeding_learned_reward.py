905e1b5c55e1cbe5b1b0cd4375dd8890a953ddcd
Jeremy Tien <jeremy29tien@gmail.com>
2022-02-10 19:11:17 -0800

    modify NN's RL reward env for ablation stuff

--- assistive_gym/envs/feeding_learned_reward.py
+++ assistive_gym/envs/feeding_learned_reward.py
@@ -13,13 +13,14 @@ class FeedingLearnedRewardEnv(FeedingEnv):
     # Primus: /home/jtien/assistive-gym/trex/models/5000_trajs_early_stopping.params
     # With weight decay: /home/jtien/assistive-gym/trex/models/5000traj_100epoch_1weightdecay_earlystopping.params
     # With no bias: /home/jtien/assistive-gym/trex/models/5000traj_100epoch_nobias_earlystopping.params
-    # Local: /Users/jeremytien/Documents/3rd-Year/Research/Anca Dragan/assistive-gym/trex/models/test1.params    
-    reward_net_path = "/home/jtien/assistive-gym/trex/models/augmented_features/60demosallpairs_100epochs_10patience_001lr_01weightdecay_seed0.params"
-    reward_net = None
-    device = None
-
+    # Local: /Users/jeremytien/Documents/3rd-Year/Research/Anca Dragan/assistive-gym/trex/models/test1.params
     def __init__(self, robot, human):
         super(FeedingLearnedRewardEnv, self).__init__(robot=robot, human=human)
+        self.augmented = True
+        self.state_action = False
+        self.num_rawfeatures = 0
+
+        self.reward_net_path = "/home/jtien/assistive-gym/trex/models/augmented_features/ablation_0rawfeatures_10demosallpairs_10epochs_10patience_001lr_01weightdecay_seed0.params"
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.reward_net = Net(with_bias=False, augmented=True, state_action=False)
         print("device:", self.device)
@@ -30,21 +31,20 @@ class FeedingLearnedRewardEnv(FeedingEnv):
     def step(self, action):
         obs, reward, done, info = super().step(action)
 
-        # If features consist of state-action pairs:
-        # state = np.concatenate((obs, action))
-
-        # If features consist of just the observation:
-        state = obs
-
-        # If using augmented features
         distance = np.linalg.norm(obs[7:10])  # spoon_pos_real - target_pos_real is at index 7,8,9
         foods_in_mouth = info['foods_in_mouth']
         foods_on_floor = info['foods_on_ground']
-        linear_features = np.array([distance, foods_in_mouth, foods_on_floor])
-        state = np.concatenate((state, linear_features))
+        handpicked_features = np.array([distance, foods_in_mouth, foods_on_floor])
+
+        if self.augmented and self.state_action:
+            input = np.concatenate((obs, action, handpicked_features))
+        elif self.augmented:
+            input = np.concatenate((obs[0:self.num_rawfeatures], handpicked_features))
+        else:
+            input = handpicked_features
 
         # Just modify the reward
         with torch.no_grad():
-            reward = self.reward_net.cum_return(torch.from_numpy(np.array([state])).float().to(self.device)).item()
+            reward = self.reward_net.cum_return(torch.from_numpy(np.array([input])).float().to(self.device)).item()
 
         return obs, reward, done, info
