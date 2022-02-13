1cc6a349b34079db562f7e60f717d1bafe08e587
Jeremy Tien <jeremy29tien@gmail.com>
2022-02-12 16:13:39 -0800

    bug fix

--- assistive_gym/envs/feeding_learned_reward.py
+++ assistive_gym/envs/feeding_learned_reward.py
@@ -20,7 +20,7 @@ class FeedingLearnedRewardEnv(FeedingEnv):
         self.state_action = False
         self.num_rawfeatures = 25
 
-        self.reward_net_path = "/home/jtien/assistive-gym/trex/models/raw_states/100prefs_0pairdelta_100epochs_10patience_001lr_01weightdecay_seed0.params"
+        self.reward_net_path = "/home/jtien/assistive-gym/trex/models/raw_states/100prefs_100epochs_10patience_001lr_01weightdecay_seed0.params"
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.reward_net = Net(with_bias=False, augmented=self.augmented, num_rawfeatures=self.num_rawfeatures, state_action=self.state_action)
         print("device:", self.device)
