a6e378c84fbcad9279290f21df18311d08422168
Jeremy Tien <jeremy29tien@gmail.com>
2022-02-17 22:54:39 -0800

    set up for model capacity exp

--- assistive_gym/pref_learn.sh
+++ assistive_gym/pref_learn.sh
@@ -6,11 +6,11 @@ for seed in 0 1 2; do
   echo Seed $seed
 
   #Reward-learning
-  config="capacity_experiment/linear_2000prefs_60pairdelta_100epochs_10patience_001lr_001weightdecay_001l1reg"
+  config="capacity_experiment/linear_privileged_2000prefs_60pairdelta_100epochs_10patience_001lr_001weightdecay_001l1reg"
   reward_model_path="trex/models/${config}_seed${seed}.params"
   reward_output_path="trex/reward_learning_outputs/${config}_seed${seed}.txt"
 
-  python3 trex/linear_model.py --num_comps 2000 --pair_delta 60 --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.01 --l1_reg 0.01 --seed $seed --test --reward_model_path $reward_model_path > $reward_output_path
+  python3 trex/linear_model.py --augmented --num_rawfeatures 0 --num_comps 2000 --pair_delta 60 --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.01 --l1_reg 0.01 --seed $seed --test --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
   policy_save_dir="./trained_models_reward_learning/${config}_seed${seed}"
