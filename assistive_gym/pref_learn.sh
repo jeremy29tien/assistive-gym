6de221e2a0f0563877ed499f4045b7f260f453e1
Jeremy Tien <jeremy29tien@gmail.com>
2022-02-17 22:58:28 -0800

    make changes for some directory moving

--- assistive_gym/pref_learn.sh
+++ assistive_gym/pref_learn.sh
@@ -7,12 +7,14 @@ for seed in 0 1 2; do
 
   #Reward-learning
   config="capacity_experiment/linear_privileged_2000prefs_60pairdelta_100epochs_10patience_001lr_001weightdecay_001l1reg"
-  reward_model_path="trex/models/${config}_seed${seed}.params"
-  reward_output_path="trex/reward_learning_outputs/${config}_seed${seed}.txt"
+  reward_model_path="models/${config}_seed${seed}.params"
+  reward_output_path="reward_learning_outputs/${config}_seed${seed}.txt"
 
-  python3 trex/linear_model.py --augmented --num_rawfeatures 0 --num_comps 2000 --pair_delta 60 --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.01 --l1_reg 0.01 --seed $seed --reward_model_path $reward_model_path > $reward_output_path
+  cd trex/
+  python3 linear_model.py --augmented --num_rawfeatures 0 --num_comps 2000 --pair_delta 60 --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.01 --l1_reg 0.01 --seed $seed --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
+  cd ..
   policy_save_dir="./trained_models_reward_learning/${config}_seed${seed}"
   python3 -m assistive_gym.learn --env "FeedingLinearRewardSawyer-v0" --algo ppo --seed $seed --train --train-timesteps 1000000 --reward-net-path $reward_model_path --save-dir $policy_save_dir
 
