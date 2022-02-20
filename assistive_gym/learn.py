c26353bf034dda2aed1bbb0a67abe2b89fa69d4b
Jeremy Tien <jeremy@svm.bair.berkeley.edu>
2022-02-19 21:02:53 -0800

    FIGURE OUT HOW TO MAKE PPOTRAINER WORK WITH CUSTOM ENV ARGS

--- assistive_gym/learn.py
+++ assistive_gym/learn.py
@@ -37,10 +37,10 @@ def setup_config(env, algo, coop=False, seed=0, extra_configs={}):
     return {**config, **extra_configs}
 
 def load_policy(env, algo, env_name, policy_path=None, coop=False, seed=0, reward_net_path=None, extra_configs={}):
+    # reward_net_path = None  # temp for debuggin
     if algo == 'ppo':
         if reward_net_path is not None:
-            agent = ppo.PPOTrainer(setup_config(env, algo, coop, seed, extra_configs), 'assistive_gym:' + env_name,
-                                   reward_net_path=reward_net_path)  # 'assistive_gym:'+env_name
+            agent = ppo.PPOTrainer(setup_config(env, algo, coop, seed, extra_configs), 'assistive_gym:' + env_name)  # 'assistive_gym:'+env_name
         else:
             agent = ppo.PPOTrainer(setup_config(env, algo, coop, seed, extra_configs), 'assistive_gym:'+env_name)  # 'assistive_gym:'+env_name
     elif algo == 'sac':
@@ -88,7 +88,7 @@ def make_env(env_name, coop=False, seed=1001, reward_net_path=None):
 def train(env_name, algo, timesteps_total=1000000, save_dir='./trained_models/', load_policy_path='', coop=False, seed=0, save_checkpoints=False, reward_net_path=None, extra_configs={}):
     ray.init(num_cpus=multiprocessing.cpu_count(), ignore_reinit_error=True, log_to_driver=False)
     env = make_env(env_name, coop, reward_net_path=reward_net_path)
-    agent, checkpoint_path = load_policy(env, algo, env_name, load_policy_path, coop, seed, reward_net_path, extra_configs)
+    agent, checkpoint_path = load_policy(env, algo, env_name, load_policy_path, coop, seed, reward_net_path, extra_configs={"env_config": {"reward_net_path":reward_net_path}}) # extra_configs={'reward_net_path':reward_net_path}
     env.disconnect()
 
     timesteps = 0
