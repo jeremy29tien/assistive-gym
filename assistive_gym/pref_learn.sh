0bdeaf4d1b1002d6da8a15bea17d762ced0971d2
Jeremy Tien <jeremy29tien@gmail.com>
2022-02-24 20:43:04 -0800

    remove --test flag

--- assistive_gym/pref_learn.sh
+++ assistive_gym/pref_learn.sh
@@ -18,7 +18,7 @@ for seed in 0 1 2; do
   reward_output_path="reward_learning_outputs/${config}_seed${seed}.txt"
 
   cd trex/
-  python3 model.py ${var1} --augmented --hidden_dims 0 --num_demos 10 --all_pairs --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.01 --l1_reg 0.01 --seed $seed --test --reward_model_path $reward_model_path > $reward_output_path
+  python3 model.py ${var1} --augmented --hidden_dims 0 --num_demos 10 --all_pairs --num_epochs 100 --patience 10 --lr 0.01 --weight_decay 0.01 --l1_reg 0.01 --seed $seed --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
   echo "Performing RL..."
