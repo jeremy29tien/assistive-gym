85a728b918411ad95f069209505747e0826f9159
Jeremy Tien <jeremy29tien@gmail.com>
2022-06-10 16:19:50 -0700

    add features to make envs fully observable

--- assistive_gym/envs/scratch_itch.py
+++ assistive_gym/envs/scratch_itch.py
@@ -38,9 +38,9 @@ class ScratchItchEnv(AssistiveEnv):
         info = {'total_force_on_human': self.total_force_on_human, 'task_success': int(self.task_success >= self.config('task_success_threshold')),
                 'action_robot_len': self.action_robot_len, 'action_human_len': self.action_human_len,
                 'obs_robot_len': self.obs_robot_len, 'obs_human_len': self.obs_human_len,
-                'tool_force_at_target': self.tool_force_at_target, 'prev_spoon_pos_real': self.prev_spoon_pos_real,
+                'tool_force_at_target': self.tool_force_at_target, 'prev_tool_pos_real': self.prev_tool_pos_real,
                 'robot_force_on_human': self.robot_force_on_human, 'prev_tool_force': self.prev_tool_force}
-        self.prev_spoon_pos_real = obs[0:3]
+        self.prev_tool_pos_real = obs[0:3]
         self.prev_tool_force = obs[-1]
         done = self.iteration >= 200
 
@@ -109,7 +109,7 @@ class ScratchItchEnv(AssistiveEnv):
         super(ScratchItchEnv, self).reset()
         self.build_assistive_env('wheelchair')
         self.prev_target_contact_pos = np.zeros(3)
-        self.prev_spoon_pos_real = np.zeros(3)
+        self.prev_tool_pos_real = np.zeros(3)
         self.prev_tool_force = 0
         if self.robot.wheelchair_mounted:
             wheelchair_pos, wheelchair_orient = self.furniture.get_base_pos_orient()
