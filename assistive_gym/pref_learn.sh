fe6c6f73c978e14cf17c8633b3dd65ddcda9baa7
Jeremy Tien <jeremy29tien@gmail.com>
2022-07-08 15:43:13 -0700

    change for adding more seeds to vanilla pref learning fully observable

--- assistive_gym/pref_learn.sh
+++ assistive_gym/pref_learn.sh
@@ -8,12 +8,12 @@ for seed in 4 5; do
 
   #Reward-learning
   echo "Reward learning..."
-  config="feeding/vanilla/pure_fully_observable/${var1}demos_allpairs_hdim128-64_100epochs_10patience_0001lr_000001weightdecay"
+  config="feeding/vanilla/${var1}demos_hdim128-64_fullyobservable_allpairs_100epochs_10patience_0001lr_000001weightdecay"
   reward_model_path="/home/jeremy/assistive-gym/trex/models/${config}_seed${seed}.params"
   reward_output_path="reward_learning_outputs/${config}_seed${seed}.txt"
 
   cd trex/
-  python3 model.py --feeding --seed $seed --pure_fully_observable --hidden_dims 128 64 --num_demos ${var1} --all_pairs --num_epochs 100 --patience 10 --lr 0.001 --weight_decay 0.00001 --reward_model_path $reward_model_path > $reward_output_path
+  python3 model.py --feeding --seed $seed --fully_observable --hidden_dims 128 64 --num_demos ${var1} --all_pairs --num_epochs 100 --patience 10 --lr 0.001 --weight_decay 0.00001 --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
   echo "Performing RL..."
