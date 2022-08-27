cf6f283f8de6696a1c553cd28a73ec6d7456ef5d
Jeremy Tien <jeremy29tien@gmail.com>
2022-08-27 01:38:51 -0700

    make changes for feeding with larger model capacity

--- assistive_gym/pref_learn.sh
+++ assistive_gym/pref_learn.sh
@@ -8,26 +8,26 @@ for seed in 0 1 2; do
 
   #Reward-learning
   echo "Reward learning..."
-  config="scratch_itch/vanilla/new_fully_observable/${var1}demos_allpairs_hdim128-64_100epochs_10patience_000001lr_00001weightdecay"
+  config="feeding/vanilla/pure_fully_observable/${var1}demos_allpairs_hdim256-256-256_100epochs_10patience_00001lr_0000001weightdecay"
   reward_model_path="/home/jeremy/assistive-gym/trex/models/${config}_seed${seed}.params"
   reward_output_path="reward_learning_outputs/${config}_seed${seed}.txt"
 
   cd trex/
-  python3 model.py --scratch_itch --seed $seed --new_fully_observable --hidden_dims 128 64 --num_demos ${var1} --all_pairs --num_epochs 100 --patience 10 --lr 0.00001 --weight_decay 0.0001 --reward_model_path $reward_model_path > $reward_output_path
+  python3 model.py --feeding --seed $seed --pure_fully_observable --hidden_dims 256 256 256 --num_demos ${var1} --all_pairs --num_epochs 100 --patience 10 --lr 0.0001 --weight_decay 0.000001 --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
   echo "Performing RL..."
   cd ..
   policy_save_dir="./trained_models_reward_learning/${config}_seed${seed}"
-  python3 -m assistive_gym.learn --env "ScratchItchLearnedRewardJaco-v0" --algo ppo --seed $seed --train --train-timesteps 1000000 --reward-net-path $reward_model_path --save-dir $policy_save_dir --load-policy-path $policy_save_dir --tb
+  python3 -m assistive_gym.learn --env "FeedingLearnedRewardSawyer-v0" --algo ppo --seed $seed --train --train-timesteps 1000000 --reward-net-path $reward_model_path --save-dir $policy_save_dir --load-policy-path $policy_save_dir --tb
 
   #Eval
   echo "Evaluating RL..."
-  load_policy_path="${policy_save_dir}/ppo/ScratchItchLearnedRewardJaco-v0/checkpoint_40/checkpoint-40"
+  load_policy_path="${policy_save_dir}/ppo/FeedingLearnedRewardSawyer-v0/checkpoint_40/checkpoint-40"
   gt_eval_path="trex/rl/eval/${config}_seed${seed}.txt"
   learned_eval_path="trex/rl/eval/${config}_seed${seed}_learnedreward.txt"
-  python3 -m assistive_gym.learn --env "ScratchItchJaco-v1" --algo ppo --evaluate --eval-episodes 100 --seed 3 --verbose --load-policy-path $load_policy_path > $gt_eval_path
-  python3 -m assistive_gym.learn --env "ScratchItchLearnedRewardJaco-v0" --reward-net-path $reward_model_path --algo ppo --evaluate --eval-episodes 100 --seed 3 --verbose --load-policy-path $load_policy_path > $learned_eval_path
+  python3 -m assistive_gym.learn --env "FeedingSawyer-v1" --algo ppo --evaluate --eval-episodes 100 --seed 3 --verbose --load-policy-path $load_policy_path > $gt_eval_path
+  python3 -m assistive_gym.learn --env "FeedingLearnedRewardSawyer-v0" --reward-net-path $reward_model_path --algo ppo --evaluate --eval-episodes 100 --seed 3 --verbose --load-policy-path $load_policy_path > $learned_eval_path
 done
 
 
