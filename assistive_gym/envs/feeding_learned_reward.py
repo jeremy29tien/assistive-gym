434710357d52532296d11f60cec116aa81aaa20a
Jeremy Tien <jeremy29tien@gmail.com>
2022-11-10 23:31:40 -0800

    debug

--- assistive_gym/envs/feeding_learned_reward.py
+++ assistive_gym/envs/feeding_learned_reward.py
@@ -16,9 +16,9 @@ class FeedingLearnedRewardEnv(FeedingEnv):
 
         # Reward Model Specifications
         self.pure_fully_observable = False
-        self.fully_observable = False
+        self.fully_observable = True
         self.augmented = False
-        self.state_action = True
+        self.state_action = False
         self.num_rawfeatures = 25  # Feeding has 25 raw features total
         self.hidden_dims = (256, 256, 256)
         self.normalize = False
