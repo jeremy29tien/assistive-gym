c63085f7879e9141d80177cdd260daa1854c9f3f
Jeremy Tien <jeremy29tien@gmail.com>
2022-11-10 22:13:08 -0800

    make changes for noise analysis

--- assistive_gym/pref_learn.sh
+++ assistive_gym/pref_learn.sh
@@ -8,13 +8,13 @@ for seed in 0 1 2; do
 
   #Reward-learning
   echo "Reward learning..."
-  config="feeding/vanilla/40demos_allpairs_hdim256-256-256_100epochs_10patience_0001lr_0001weightdecay"
+  config="feeding/vanilla/${var2}${var1}_324demos_allpairs_hdim256-256-256_100epochs_10patience_00001lr_0000001weightdecay"
 
   reward_model_path="/home/jeremy/assistive-gym/trex/models/${config}_seed${seed}.params"
   reward_output_path="reward_learning_outputs/${config}_seed${seed}.txt"
 
   cd trex/
-  python3 model.py --feeding --seed $seed --fully_observable --hidden_dims 256 256 256 --num_demos 40 --all_pairs --num_epochs 100 --patience 10 --lr 0.001 --weight_decay 0.001 --reward_model_path $reward_model_path > $reward_output_path
+  python3 model.py --feeding --seed $seed --$var1 $var2 --fully_observable --hidden_dims 256 256 256 --num_demos 324 --all_pairs --num_epochs 100 --patience 10 --lr 0.0001 --weight_decay 0.000001 --reward_model_path $reward_model_path > $reward_output_path
 
   #RL
   echo "Performing RL..."
